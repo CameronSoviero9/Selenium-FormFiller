1ea04de6255f3d22a6c54a13e0e9c133907106ac
Cameron Soviero <camsoviero@att.net>
2022-01-28 14:12:28 -0500

    first page of form filled out

--- source_code.py
+++ source_code.py
@@ -1,17 +1,27 @@
-from selenium import webdriver      # import the webdriver application from the Selenium library
-import time                         # useful for wait functions -- dependent on connection speed
+from selenium import webdriver      # Import the webdriver application from the Selenium library
+import time                         # Useful for wait functions -- dependent on connection speed
 
 ########## CONSTANTS #########
-website_location = webdriver.Chrome()       # set to chrome for selenium acces
-website_location.get('https://forms.gle/56ARW1Wugs1KXpXo8')     # enter URL of form to fill out
+website_location = webdriver.Chrome()       # Set to chrome for selenium acces
+website_location.get('https://forms.gle/56ARW1Wugs1KXpXo8')     # Enter URL of form to fill out
 
 your_name = 'My Name'
+your_birthday = '01/01/2000'
 
 ########## Functions #########
 def main():
     # Start filling out form here
-    email_location = website_location.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
-    email_location.send_keys(your_name)
+    # Enter your name into the short answer box
+    name_location = website_location.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    name_location.send_keys(your_name)
+
+    # Enter your birthday into the data option box
+    birthday_location = website_location.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/input')
+    birthday_location.send_keys(your_birthday)
+
+    # Select the bubble to indicate your gender
+    gender_location = website_location.find_element_by_xpath('//*[@id="i14"]/div[3]/div')
+    gender_location.click()
 
 ########## MAIN #########
 if __name__ == "__main__":
